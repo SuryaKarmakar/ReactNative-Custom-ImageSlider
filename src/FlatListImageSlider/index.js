71bfa33769f3482a4d007eb7b93e17a2e4180874
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2022-01-09 23:53:45 +0530

    adding flatlist slider code

---
+++ src/FlatListImageSlider/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Styles from './styles';
+
+const FlatListImageSlider = props => {
+  const {data} = props;
+
+  return <View></View>;
+};
+export default FlatListImageSlider;
---
+++ src/FlatListImageSlider/styles.js
@@ -0,0 +1,7 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+const Styles = StyleSheet.create({});
+
+export default Styles;
